865327a2d7bc4ecc0b734978a968c715c522b604
KellenMortensen <kellen.mortensen@student.northampton.edu>
2026-02-04 20:22:08 +0000

    Salesperson paycheck project

--- main.cpp
+++ main.cpp
@@ -1,5 +1,71 @@
+// Kellen Mortensen
+
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+
+  string firstName, lastName;
+  string filename;
+
+  double baseSalary;
+  double commissionPercent;
+  double totalSales;
+  double employeeExpenses;
+
+  cout << "Please enter input file name: ";
+  cin >> filename;
+
+  ifstream inputFile(filename);
+
+  inputFile >> firstName >> lastName;
+  inputFile >> baseSalary >> commissionPercent;
+  inputFile >> totalSales;
+  inputFile >> employeeExpenses; 
+  inputFile.close();
+
+  double commissionAmount = totalSales*(commissionPercent/100.0);
+  double payAmount = baseSalary + commissionAmount - employeeExpenses;
+
+  cout << setprecision(2);
+
+  cout << endl;
+  cout << "Payroll data for " << firstName << " " << lastName << endl;
+
+  cout  << "Base Salary: " << endl;
+
+
+
+  cout << "Commission: " << endl;
+
+
+
+  cout << "Expenses: " << endl;
+
+  
+
+  cout << "Total: " << endl;
+
+  return 0;
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
 }
